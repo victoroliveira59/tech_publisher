990f88854e3ec17f92bb4ab3c33265adf05007c3
victoroliveira59 <victorsamueloliveira59@gmail.com>
2023-10-29 16:24:10 -0300

    chore(controller): "Modified the controller to exclude JSON responses."

--- app/controllers/accounts_controller.rb
+++ app/controllers/accounts_controller.rb
@@ -19,32 +19,27 @@ class AccountsController < ApplicationController
   def edit
   end
 
-  # POST /accounts or /accounts.json
-  def create
+  # POST /accounts or /accounts.js
     @account = Account.new(account_params)
 
-    respond_to do |format|
       if @account.save
-        format.html { redirect_to account_url(@account), notice: "Account was successfully created." }
-        format.json { render :show, status: :created, location: @account }
+         redirect_to account_url(@account), notice: "Account was successfully created."
+         render :show, status: :created, location: @account
       else
-        format.html { render :new, status: :unprocessable_entity }
-        format.json { render json: @account.errors, status: :unprocessable_entity }
+         render :new, status: :unprocessable_entity
+         render json: @account.errors, status: :unprocessable_entity
       end
-    end
   end
 
   # PATCH/PUT /accounts/1 or /accounts/1.json
   def update
-    respond_to do |format|
+
       if @account.update(account_params)
-        format.html { redirect_to account_url(@account), notice: "Account was successfully updated." }
-        format.json { render :show, status: :ok, location: @account }
+         redirect_to account_url(@account), notice: "Account was successfully updated."
+         render :show, status: :ok, location: @account
       else
-        format.html { render :edit, status: :unprocessable_entity }
-        format.json { render json: @account.errors, status: :unprocessable_entity }
+         render :edit, status: :unprocessable_entity
       end
-    end
   end
 
   # DELETE /accounts/1 or /accounts/1.json
@@ -52,9 +47,8 @@ class AccountsController < ApplicationController
     @account.destroy
 
     respond_to do |format|
-      format.html { redirect_to accounts_url, notice: "Account was successfully destroyed." }
-      format.json { head :no_content }
-    end
+       redirect_to accounts_url, notice: "Account was successfully destroyed."
+       head :no_content
   end
 
   private
