68cd1bd68be626ad140e87c41099b01636d40ea1
victoroliveira59 <victorsamueloliveira59@gmail.com>
2023-10-29 16:45:50 -0300

    feat(controller): "Modified the controller to exclude JSON responses."

--- app/controllers/parts_controller.rb
+++ app/controllers/parts_controller.rb
@@ -1,5 +1,7 @@
+# frozen_string_literal: true
+
 class PartsController < ApplicationController
-  before_action :set_part, only: %i[ show edit update destroy ]
+  before_action :set_part, only: %i[show edit update destroy]
 
   # GET /parts or /parts.json
   def index
@@ -7,8 +9,7 @@ class PartsController < ApplicationController
   end
 
   # GET /parts/1 or /parts/1.json
-  def show
-  end
+  def show; end
 
   # GET /parts/new
   def new
@@ -16,49 +17,47 @@ class PartsController < ApplicationController
   end
 
   # GET /parts/1/edit
-  def edit
-  end
+  def edit; end
 
   # POST /parts or /parts.json
   def create
     @part = Part.new(part_params)
 
-      if @part.save
-        fredirect_to part_url(@part), notice: "Part was successfully created."
-         render :show, status: :created, location: @part
-      else
-         render :new, status: :unprocessable_entity
-      end
+    if @part.save
+      fredirect_to part_url(@part), notice: 'Part was successfully created.'
+      render :show, status: :created, location: @part
+    else
+      render :new, status: :unprocessable_entity
+    end
   end
 
   # PATCH/PUT /parts/1 or /parts/1.json
   def update
-
-      if @part.update(part_params)
-         redirect_to part_url(@part), notice: "Part was successfully updated."
-         render :show, status: :ok, location: @part
-      else
-         render :edit, status: :unprocessable_entity
-      end
+    if @part.update(part_params)
+      redirect_to part_url(@part), notice: 'Part was successfully updated.'
+      render :show, status: :ok, location: @part
+    else
+      render :edit, status: :unprocessable_entity
+    end
   end
 
   # DELETE /parts/1 or /parts/1.json
   def destroy
     @part.destroy
 
-
-       redirect_to parts_url, notice: "Part was successfully destroyed."
-       head :no_content
+    redirect_to parts_url, notice: 'Part was successfully destroyed.'
+    head :no_content
   end
 
   private
-    # Use callbacks to share common setup or constraints between actions.
-    def set_part
-      @part = Part.find(params[:id])
-    end
 
-    # Only allow a list of trusted parameters through.
-    def part_params
-      params.require(:part).permit(:name, :description, :supplier_id)
-    end
+  # Use callbacks to share common setup or constraints between actions.
+  def set_part
+    @part = Part.find(params[:id])
+  end
+
+  # Only allow a list of trusted parameters through.
+  def part_params
+    params.require(:part).permit(:name, :description, :supplier_id)
+  end
 end
--- app/controllers/suppliers_controller.rb
+++ app/controllers/suppliers_controller.rb
@@ -1,5 +1,7 @@
+# frozen_string_literal: true
+
 class SuppliersController < ApplicationController
-  before_action :set_supplier, only: %i[ show edit update destroy ]
+  before_action :set_supplier, only: %i[show edit update destroy]
 
   # GET /suppliers or /suppliers.json
   def index
@@ -7,8 +9,7 @@ class SuppliersController < ApplicationController
   end
 
   # GET /suppliers/1 or /suppliers/1.json
-  def show
-  end
+  def show; end
 
   # GET /suppliers/new
   def new
@@ -16,47 +17,47 @@ class SuppliersController < ApplicationController
   end
 
   # GET /suppliers/1/edit
-  def edit
-  end
+  def edit; end
 
   # POST /suppliers or /suppliers.json
   def create
     @supplier = Supplier.new(supplier_params)
 
-      if @supplier.save
-         redirect_to supplier_url(@supplier), notice: "Supplier was successfully created."
-         render :show, status: :created, location: @supplier
-      else
-         render :new, status: :unprocessable_entity
-      end
+    if @supplier.save
+      redirect_to supplier_url(@supplier), notice: 'Supplier was successfully created.'
+      render :show, status: :created, location: @supplier
+    else
+      render :new, status: :unprocessable_entity
+    end
   end
 
   # PATCH/PUT /suppliers/1 or /suppliers/1.json
   def update
-      if @supplier.update(supplier_params)
-         redirect_to supplier_url(@supplier), notice: "Supplier was successfully updated."
-         render :show, status: :ok, location: @supplier
-      else
-         render :edit, status: :unprocessable_entity
-      end
+    if @supplier.update(supplier_params)
+      redirect_to supplier_url(@supplier), notice: 'Supplier was successfully updated.'
+      render :show, status: :ok, location: @supplier
+    else
+      render :edit, status: :unprocessable_entity
+    end
   end
 
   # DELETE /suppliers/1 or /suppliers/1.json
   def destroy
     @supplier.destroy
 
-       redirect_to suppliers_url, notice: "Supplier was successfully destroyed."
-       head :no_content
+    redirect_to suppliers_url, notice: 'Supplier was successfully destroyed.'
+    head :no_content
   end
 
   private
-    # Use callbacks to share common setup or constraints between actions.
-    def set_supplier
-      @supplier = Supplier.find(params[:id])
-    end
 
-    # Only allow a list of trusted parameters through.
-    def supplier_params
-      params.require(:supplier).permit(:name)
-    end
+  # Use callbacks to share common setup or constraints between actions.
+  def set_supplier
+    @supplier = Supplier.find(params[:id])
+  end
+
+  # Only allow a list of trusted parameters through.
+  def supplier_params
+    params.require(:supplier).permit(:name)
+  end
 end
