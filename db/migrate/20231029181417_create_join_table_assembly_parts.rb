05d7cb4c6c617d7ba7d65118a013bfe5c1af1a44
victoroliveira59 <victorsamueloliveira59@gmail.com>
2023-10-29 16:11:15 -0300

    build(references): CreateJoinTable

--- db/migrate/20231029181417_create_join_table_assembly_parts.rb
+++ db/migrate/20231029181417_create_join_table_assembly_parts.rb
@@ -1,8 +1,8 @@
 class CreateJoinTableAssemblyParts < ActiveRecord::Migration[7.0]
   def change
     create_join_table :assemblies, :parts do |t|
-      # t.index [:assembly_id, :part_id]
-      # t.index [:part_id, :assembly_id]
+      t.index [:assembly_id, :part_id]
+      t.index [:part_id, :assembly_id]
     end
   end
 end
