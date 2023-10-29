6f095e1d1cf65ce7e995824762205bebcf8b79bd
victoroliveira59 <victorsamueloliveira59@gmail.com>
2023-10-29 15:51:05 -0300

    feat: Update schema

--- db/schema.rb
+++ db/schema.rb
@@ -29,17 +29,6 @@ ActiveRecord::Schema[7.0].define(version: 2023_10_29_181227) do
     t.datetime "updated_at", null: false
   end
 
-  create_table "assemblies_parts", id: false, force: :cascade do |t|
-    t.bigint "assembly_id", null: false
-    t.bigint "part_id", null: false
-  end
-
-  create_table "assemblies", force: :cascade do |t|
-    t.string "name"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
-  end
-
   create_table "authors", force: :cascade do |t|
     t.string "name"
     t.datetime "created_at", null: false
@@ -51,18 +40,7 @@ ActiveRecord::Schema[7.0].define(version: 2023_10_29_181227) do
     t.bigint "author_id", null: false
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
-    t.bigint "assembly_id", null: false
-    t.index ["assembly_id"], name: "index_books_on_assembly_id"
     t.index ["author_id"], name: "index_books_on_author_id"
-  end
-
-  create_table "parts", force: :cascade do |t|
-    t.string "name"
-    t.text "description"
-    t.bigint "supplier_id", null: false
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
-    t.index ["supplier_id"], name: "index_parts_on_supplier_id"
   end
 
   create_table "suppliers", force: :cascade do |t|
@@ -72,7 +50,5 @@ ActiveRecord::Schema[7.0].define(version: 2023_10_29_181227) do
   end
 
   add_foreign_key "accounts", "suppliers"
-  add_foreign_key "books", "assemblies"
   add_foreign_key "books", "authors"
-  add_foreign_key "parts", "suppliers"
 end
