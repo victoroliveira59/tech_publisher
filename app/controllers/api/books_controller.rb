f132245396660656aeb677eeb96c17a297d519b8
victoroliveira59 <victorsamueloliveira59@gmail.com>
2023-11-05 14:34:08 -0300

    feat(Book API): Created

---
+++ app/controllers/api/books_controller.rb
@@ -0,0 +1,61 @@
+class Api::BooksController < ApplicationController
+  before_action :set_book, only: %i[show edit update destroy]
+  skip_before_action :verify_authenticity_token
+
+  # GET /books
+  def index
+    @books = Book.all
+    render json: @books
+  end
+
+  # GET /books/1
+  def show
+    render json: @book
+  end
+
+  # GET /books/new
+  def new
+    @book = Book.new
+  end
+
+  # GET /books/1/edit
+  def edit
+  end
+
+  # POST /books
+  def create
+    @book = Book.new(book_params)
+
+    if @book.save
+      render json: @book, status: :created
+    else
+      render json: @book.errors, status: :unprocessable_entity
+    end
+  end
+
+  # PATCH/PUT /books/1
+  def update
+    if @book.update(book_params)
+      render json: @book, status: :ok
+    else
+      render json: @book.errors, status: :unprocessable_entity
+    end
+  end
+
+  # DELETE /books/1
+  def destroy
+    @book.destroy
+    head :no_content
+  end
+
+  private
+  # Use callbacks to share common setup or constraints between actions.
+  def set_book
+    @book = Book.find(params[:id])
+  end
+
+  # Only allow a list of trusted parameters through.
+  def book_params
+    params.require(:book).permit(:title, :author_id)
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 Rails.application.routes.draw do
   namespace :api, defaults: {format: :json} do
     resources :authors
+    resources :books
   end
   resources :assemblies
   resources :parts
