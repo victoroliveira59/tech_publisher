59f596de1bc4bc694184e9c9e58030c44b621448
victoroliveira59 <victorsamueloliveira59@gmail.com>
2023-11-08 21:31:25 -0300

    feat(helper & models): "Included a 'digit' column in the account controller."

--- app/models/account.rb
+++ app/models/account.rb
@@ -1,5 +1,20 @@
 class Account < ApplicationRecord
+
   belongs_to :supplier
-  validates :number, presence: true, numericality: {greater_than: 5}
   validates :bank, presence: true
+  validates :number, length: { is: 6 }
+
+  include AccountsHelper
+
+  validates :digit, presence: true, format: { with: /[0-9a-p]/ }
+  validate :validate_digit
+
+  private
+
+  def validate_digit
+    calculated_digit = calculate_dv(number, digit)
+    if calculated_digit != 'digit is valid'
+      errors.add(:digit, 'digit invalid')
+    end
+  end
 end
